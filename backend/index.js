62c3c584f8c3bcf158963b28485b45e85d7369cd
Select-rest <vendasdobraga@gmail.com>
2024-07-10 20:06:17 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,30 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Conexão com o MongoDB
-const mongoUri = 'mongodb://localhost:27017/nome_do_banco_de_dados';
-mongoose.connect(mongoUri, {
+mongoose.connect('mongodb://localhost:27017/restaurante', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-  .then(() => console.log('Conectado ao MongoDB'))
-  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
-
-// Rotas
-app.get('/', (req, res) => {
-  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Rota inicial
+app.get('/', (req, res) => {
+  res.send('Servidor rodando');
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
